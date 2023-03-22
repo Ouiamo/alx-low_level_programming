f7a2bf59580b7f8a80b7de4c2fe6c53806427944
Ouiamo <wiamaouladelhaj2004>
2023-03-21 17:19:38 -0700

    l

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _islower - checks for lowercase character.
+ * @c: first arg.
+ * Return: Returns 1 if c is lowercase
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	return (0);
+}
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
